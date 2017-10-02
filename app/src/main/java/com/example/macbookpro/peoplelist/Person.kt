f03234893835a05f0a0ec6d2b9a7ee0f6517dbac
Marcio Zebedeu <marciozebedeu@gmail.com>
2017-10-02 11:13:25 +0100

    fix

--- app/src/main/java/com/example/macbookpro/peoplelist/Person.kt
+++ app/src/main/java/com/example/macbookpro/peoplelist/Person.kt
@@ -2,10 +2,8 @@ package com.example.macbookpro.peoplelist
 
 import java.io.Serializable
 
-/**
- * Created by macbookpro on 02/10/17.
- */
-class Person(var name: String, var email: String) : Serializable {
+
+data class Person(var name: String, var email: String) : Serializable {
 
     override fun toString(): String {
         return "${name.toLowerCase()} - $email"
