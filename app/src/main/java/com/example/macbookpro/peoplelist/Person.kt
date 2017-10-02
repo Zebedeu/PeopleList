9cf52e22549ed3d8a28be3cefb4bc1c1e3fbfc7d
Marcio Zebedeu <marciozebedeu@gmail.com>
2017-10-02 11:24:49 +0100

    update

--- app/src/main/java/com/example/macbookpro/peoplelist/Person.kt
+++ app/src/main/java/com/example/macbookpro/peoplelist/Person.kt
@@ -6,6 +6,6 @@ import java.io.Serializable
 data class Person(var name: String, var email: String) : Serializable {
 
     override fun toString(): String {
-        return "${name.toLowerCase()} - $email"
+        return "$name - $email"
     }
 }
